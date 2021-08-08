857a49cb8c8df584cd2fa2e6164e85cd9408c226
duygutuncer <duygu_tuncer1@hotmail.com>
2021-08-08 18:36:23 +0200

    Split method is applied on the argument data, to make each word in string that gets passed in the argument-data- as an array item, so that array methods can be applicable on the next steps. Arter that, filter method is applied to filter the items in the curly brackets.

--- chop.js
+++ chop.js
@@ -7,15 +7,29 @@ function chop (data, template) {
         console.log("data type is not right");
         return "wrong data type"
     } else if (typeof data == "string" && typeof template == "object") {
-        console.log("data type is right.");
-        let str = data;
-                    let cleanStr = str.substring(2, str.length - 2);
-                    console.log("str is: ", cleanStr);
-                return cleanStr;
+        // console.log("data type is right.");
+
+         const splitedData = data.split(" ");
+         console.log("splitedData", splitedData);
+      
+        let filteredData = splitedData.filter((item) => {
+            if (
+                item.slice(0, 2) === "{{" &&
+                item.slice(item.length - 2, item.length) === "}}"
+            ) {
+                return item;
+            }
+        });
+
+        console.log("filteredData:", filteredData);
+
+        return filteredData;
     }
 }
 
-chop("heyyyooooo", {});
+chop(
+    "I have many things in my fridge such as {{bread}} {{focaccio}} {{salmon}} say {{hallo}}",
+    { bread: "essential", salmon: "delicioussy", focaccio: "carby", hallo: "meal" });
 
 module.exports = chop
 
--- chop.test.js
+++ chop.test.js
@@ -1,13 +1,19 @@
 const chop = require('./chop');
 
-test('Run the test with different data types', () => {
-  expect(chop("heyyyooooo", {})).toBe("yyyooo");
-});
+// test('Run the test with different data types', () => {
+//   expect(chop("heyyyooooo", {})).toBe("yyyooo");
+// });
 
-test('Run the test with different data types', () => {
-  expect(chop(2, {})).toBe("wrong data type");
-});
+// test('Run the test with different data types', () => {
+//   expect(chop(2, {})).toBe("wrong data type");
+// });
 
-test('Run the test with different data types', () => {
-  expect(chop("heyyyooooo", null)).toBe("wrong data type");
+// test('Run the test with different data types', () => {
+//   expect(chop("heyyyooooo", null)).toBe("wrong data type");
+// });
+
+test('Run the test with filteredData', () => {
+  expect(chop("I have many things in my fridge such as {{bread}} {{focaccio}} {{salmon}} say {{hallo}}",
+    { bread: "essential", salmon: "delicioussy", focaccio: "carby", hallo: "meal" })).
+    toBe([ '{{bread}}', '{{focaccio}}', '{{salmon}}', '{{hallo}}' ]);
 });
