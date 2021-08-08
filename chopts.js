f1d8ea04dc978f3c55211b8bee58c05ec98d27a7
duygutuncer <duygu_tuncer1@hotmail.com>
2021-08-09 00:11:10 +0200

    New files chopts.ts and chopts.js is created to test the code with typeScript. Type annotation of argument -data- is defined as string, and type annotation of argument -template- is defined as object.

--- chopts.js
+++ chopts.js
@@ -1,4 +1,32 @@
-function chops(data, template) {
-    console.log("log the arguments", data, template);
+function chop(data, template) {
+    if (template === null ||
+        Array.isArray(template) ||
+        typeof template === "function") {
+        return "data type of the argument does not match with the requirements";
+    }
+    else if (typeof data == "string" && typeof template == "object") {
+        var splitedData = data.split(" ");
+        var newArray = splitedData.map(function (item) {
+            if (item.slice(0, 2) === "{{" &&
+                item.slice(item.length - 2, item.length) === "}}") {
+                var prop;
+                for (prop in template) {
+                    if (item.indexOf(prop) >= 0) {
+                        item = item.replace(item, template[prop]);
+                    }
+                }
+                return item;
+            }
+            else {
+                return item;
+            }
+        });
+        var joinedArray = newArray.join(" ");
+        console.log("joinedArray", joinedArray);
+        return joinedArray;
+    }
+    else {
+        return "data type of the argument does not match with the requirements";
+    }
 }
-chops("hey", {});
+chop(2, { bread: "essential", salmon: "delicioussy", focaccio: "carby", hallo: "meal" });
--- chopts.ts
+++ chopts.ts
@@ -1,5 +1,38 @@
-function chops(data:String, template:object) {
-    console.log("log the arguments", data, template);
+function chop (data:string, template:object) {
+    if (
+        template === null ||
+        Array.isArray(template) ||
+        typeof template === "function"
+    ) {
+        return "data type of the argument does not match with the requirements"
+    } else if (typeof data == "string" && typeof template == "object") {
+
+        const splitedData = data.split(" ");
+
+        const newArray = splitedData.map((item) =>  {
+            if (
+                item.slice(0, 2) === "{{" &&
+                item.slice(item.length - 2, item.length) === "}}") {
+                var prop;
+                for(prop in template) {
+                    if (item.indexOf(prop) >= 0){
+                        item = item.replace(item, template[prop])
+                    }
+                } 
+                return item; 
+            } else {
+                return item;
+            }
+         })
+        let joinedArray = newArray.join(" ");
+        console.log("joinedArray", joinedArray)
+        return joinedArray;
+    }
+    else {
+        return "data type of the argument does not match with the requirements"
+    }
 }
 
-chops("hey", {})
+chop(
+    "I have many things in my fridge such as {{bread}} {{focaccio}} {{salmon}} say {{hallo}}",
+    { bread: "essential", salmon: "delicioussy", focaccio: "carby", hallo: "meal" });
